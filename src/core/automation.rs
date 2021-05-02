9c50bf5e6516fe6f961d21de7c4c3a071bcdd697
iiYese <iiYese@outlook.com>
2021-05-02 21:16:35 +0100

    test push

--- src/core/automation.rs
+++ src/core/automation.rs
@@ -28,31 +28,4 @@ impl Automation {
     pub fn set_lower_bound(&mut self, v: f32) {
         if self.dynamic_bound { self.lower_bound = v; }
     }
-
-    fn lerp(&self, index: usize /*starting point*/, offset: f32) {
-        let span = self.upper_bound - self.lower_bound;
-        let points = &self.points;
-    }
-
-    pub fn start(&mut self, offset: f32) {
-        for i in (0..self.points.len()).step_by(2) {
-            if offset < self.points[i].x {
-                self.seek_cahce.index = i;
-                self.lerp(i, offset)
-            }
-        }
-        self.seek_cahce.index = self.points.len() - 1;
-        self.lerp(self.points.len() - 1, offset)
-    }
-
-    pub fn advance(&mut self, offset: f32) {
-        for i in (self.seek_cahce.index..self.points.len()).step_by(2) {
-            if offset < self.points[i].x {
-                self.seek_cahce.index = i;
-                self.lerp(i, offset)
-            }
-        }
-        self.seek_cahce.index = self.points.len() - 1;
-        self.lerp(self.points.len() - 1, offset)
-    }
 }
