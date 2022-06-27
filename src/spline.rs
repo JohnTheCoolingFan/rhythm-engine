48bd7586924231d5d3af95b4b5d4f354febf8948
iiYese <iiyese@outlook.com>
2022-06-27 01:51:33 +0100

    Added bezier sampler.

--- src/spline.rs
+++ src/spline.rs
@@ -1,6 +1,9 @@
+use std::hint::unreachable_unchecked;
+
 use bevy::prelude::*;
 use derive_more::{Deref, DerefMut, From};
 use itertools::Itertools;
+use lyon_geom::*;
 use noisy_float::prelude::*;
 
 use crate::{resources::*, utils::*};
@@ -17,7 +20,7 @@ struct Segment {
     end: Vec2,
 }
 
-pub enum CurveRender {
+pub enum SampledCurve {
     Points(Vec<(Vec2, R32)>),
     Arc {
         start: Vec2,
@@ -26,23 +29,14 @@ pub enum CurveRender {
     },
 }
 
-pub struct SegmentRender {
-    path: CurveRender,
-    start: R32,
+pub struct SampledSegment {
+    path_displacement: R32,
+    sample: SampledCurve,
 }
 
-impl SegmentRender {
-    fn length(&self) -> R32 {
-        match self.path {
-            CurveRender::Points(data) => data.last().unwrap().1,
-            CurveRender::Arc { .. } => unimplemented!(),
-        }
-    }
-}
-
-impl Quantify for SegmentRender {
+impl Quantify for SampledSegment {
     fn quantify(&self) -> R32 {
-        self.start
+        self.path_displacement
     }
 }
 
@@ -50,17 +44,69 @@ impl Quantify for SegmentRender {
 struct Spline(Vec<Segment>);
 
 #[derive(Component, Deref, DerefMut, From)]
-pub struct SplineLut(pub Vec<SegmentRender>);
+pub struct SplineLut(pub Vec<SampledSegment>);
 
 impl Spline {
+    fn sample_bezier(start: Vec2, flattened: impl Iterator<Item = Point<f32>>) -> Vec<(Vec2, R32)> {
+        [start]
+            .into_iter()
+            .chain(flattened.map(|p| Vec2::new(p.x, p.y)))
+            .tuple_windows::<(_, _)>()
+            .scan(r32(0.), |segment_length, (prev, current)| {
+                *segment_length += r32(prev.distance(current));
+                Some((current, *segment_length))
+            })
+            .collect()
+    }
+
     #[rustfmt::skip]
-    fn render(&self) -> SplineLut {
+    fn sample(&self) -> SplineLut {
+
         [Segment { curvature: Curvature::Linear, end: Vec2::new(0., 0.) }]
             .iter()
             .chain(self.iter())
             .tuple_windows::<(_, _)>()
-            .scan(r32(0.), |length, (prev, current)| {
-                unimplemented!()
+            .scan(r32(0.), |path_length, (Segment { end: start, .. }, Segment { curvature, end })| {
+                Some(SampledSegment {
+                    path_displacement: *path_length,
+                    sample: match curvature {
+                        Curvature::Linear => {
+                            let segment_length = r32(start.distance(*end));
+                            *path_length += segment_length;
+
+                            SampledCurve::Points(vec![
+                                (*start, r32(0.)),
+                                (*end, segment_length)
+                            ])
+                        }
+                        Curvature::Circular(ctrl) => {
+                            unimplemented!()
+                        }
+                        Curvature::Quadratic(ctrl) => {
+                            let bezier = QuadraticBezierSegment {
+                                from: start.to_array().into(),
+                                ctrl: ctrl.to_array().into(),
+                                to: end.to_array().into()
+                            };
+
+                            let sampled = Self::sample_bezier(*start, bezier.flattened(0.05));
+                            *path_length += sampled.last().unwrap().1;
+                            SampledCurve::Points(sampled)
+                        }
+                        Curvature::Cubic(a, b) => {
+                            let bezier = CubicBezierSegment {
+                                from: start.to_array().into(),
+                                ctrl1: a.to_array().into(),
+                                ctrl2: b.to_array().into(),
+                                to: end.to_array().into()
+                            };
+
+                            let sampled = Self::sample_bezier(*start, bezier.flattened(0.05));
+                            *path_length += sampled.last().unwrap().1;
+                            SampledCurve::Points(sampled)
+                        }
+                    }
+                })
             })
             .collect::<Vec<_>>()
             .into()
